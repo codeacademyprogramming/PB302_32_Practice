cd510a5a3fbcb572da7cff2869d7d7836bc143ca
eaghayev <eaghayevv@gmail.com>
2024-02-08 21:50:05 +0400

    project added

---
+++ 08022024/Homework/Data.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Data
+    {
+        public Data()
+        {
+            Seed();
+        }
+        private const string dataPath = "C:\\Users\\elchin\\Desktop\\CodeLessons\\32. 08-02-2024\\08022024\\Homework\\DataFiles\\";
+        public List<TodoItem> TodoItems => GetTodoItems();
+
+        private List<TodoItem> GetTodoItems()
+        {
+            using (FileStream fs = new FileStream(dataPath + "todoitems.json", FileMode.Open))
+            {
+                StreamReader sr = new StreamReader(fs);
+                string jsonStr = sr.ReadToEnd();
+                sr.Close();
+
+                var data = JsonSerializer.Deserialize<List<TodoItem>>(jsonStr,new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+                return data;
+            }
+        }
+
+        private void Seed()
+        {
+            if (!File.Exists(dataPath + "todoitems.json"))
+            {
+                List<TodoItem> items = new List<TodoItem>()
+            {
+                new TodoItem{Id=1,Title="Title 1",Completed=false},
+                new TodoItem{Id=2,Title="Title 2",Completed=false},
+                new TodoItem{Id=3,Title="Title 3",Completed=true},
+                new TodoItem{Id=4,Title="Title 4",Completed=false},
+            };
+                SaveTodoItems(items);
+            }
+
+        }
+
+        public void SaveTodoItems(List<TodoItem> items)
+        {
+            var jsonStr = JsonSerializer.Serialize(items,new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            using (FileStream fs = new FileStream(dataPath + "todoitems.json", FileMode.Create))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(jsonStr);
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ 08022024/Homework/Program.cs
@@ -0,0 +1,110 @@
+﻿
+using Homework;
+using System.Net;
+using System.Text.Json;
+
+Data data = new Data();
+var todoList = data.TodoItems;
+
+string opt;
+do
+{
+    Console.WriteLine("\n=======menu======");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Remove");
+    Console.WriteLine("3. ShowAll");
+    Console.WriteLine("4. Show all external");
+
+    Console.WriteLine("5. Add an external todoItem");
+    Console.WriteLine("0. Exit");
+    opt = Console.ReadLine();
+
+    switch (opt)
+    {
+        case "1":
+            Console.WriteLine("Title: ");
+            string title  = Console.ReadLine();
+
+            Console.WriteLine("is done:");
+            string isDoneStr = Console.ReadLine();
+            bool isDone = Convert.ToBoolean(isDoneStr);
+
+            TodoItem newTodo = new TodoItem
+            {
+                Title = title,
+                Completed = isDone,
+                Id = todoList.Max(x=>x.Id)+1
+            };
+
+            todoList.Add(newTodo);
+            data.SaveTodoItems(todoList);
+
+            break;
+        case "2":
+            break;
+        case "3":
+            Console.WriteLine("\n\tAll todo items");
+            foreach (var item in todoList)
+            {
+                Console.WriteLine(item.Id+"-"+item.Title+"-"+item.Completed);
+            }
+            break;
+        case "4":
+            HttpClient client = new HttpClient();
+            var externalDataStr = client.GetStringAsync("https://jsonplaceholder.typicode.com/todos").Result;
+
+            List<TodoItem> externalData = JsonSerializer.Deserialize<List<TodoItem>>(externalDataStr, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            Console.WriteLine("\n\tExternal todo items:");
+            foreach (var item in externalData)
+            {
+                Console.WriteLine(item.Id + "-" + item.Title + "-" + item.Completed);
+            }
+            break;
+        case "5":
+            Console.WriteLine("Ecternal todo id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            if(todoList.Exists(x => x.Id == id))
+            {
+                Console.WriteLine("todo item is already added by id: "+id);
+                break;
+            }
+
+            using (client = new HttpClient())
+            {
+                var response = client.GetAsync("https://jsonplaceholder.typicode.com/todos/" + id).Result;
+
+                if(response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("Todo item not found by id:"+id);
+                    break;
+                }
+
+                var dataStr = response.Content.ReadAsStringAsync().Result;
+                TodoItem externalTodo = JsonSerializer.Deserialize<TodoItem>(dataStr, new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+
+                todoList.Add(externalTodo);
+                data.SaveTodoItems(todoList);
+            }
+
+            break;
+        case "0":
+            break;
+        default:
+            break;
+    }
+}
+while (opt != "0");
+
+
+
+
+
+
